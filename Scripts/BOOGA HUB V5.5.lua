ddf8b49128d8af01823f783213426f51dc94b51f
booga41 <141683320+booga41@users.noreply.github.com>
2023-08-08 03:25:22 +0200

    Update BOOGA HUB V5.5.lua

--- Scripts/BOOGA HUB V5.5.lua
+++ Scripts/BOOGA HUB V5.5.lua
@@ -4,4 +4,4 @@ if not game:IsLoaded() then
     game.Loaded:Wait()
 end
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/FortniBloxYT1/BOOGA-HUB-V4/main/BOOGA-HUB%20V5.lua%22"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FortniBloxYT1/BOOGA-HUB-V4/main/BOOGA-HUB%20V5.lua"))()
