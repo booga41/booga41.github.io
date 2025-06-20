2076ab69ddc21d2321659eeab4a8a293296044e7
booga41 <141683320+booga41@users.noreply.github.com>
2025-06-20 20:43:39 +0200

    Add files via upload

---
+++ Scripts/Booga Hub Blue Lock Free.lua
@@ -0,0 +1,3 @@
+-- [[ BOOGA HUB BLUE LOCK FREE VERSION | discord.gg/universenile TO BUY THE PREMIUM VERSION ]]
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/FortniBloxYT1/Booga-Hub-Blue-Lock-Free/refs/heads/main/Booga%20Key%20System%20Blue%20Lock"))()
